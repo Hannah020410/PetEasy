393e38f2f2a49256e11e6e74ac3a5a16f4c44542
Cyril Yen <cyril_yen@bizlinktech.com>
2018-07-19 16:28:47 +0800

    first commit

---
+++ DbRepository/TableClass/TableStandard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbRepository.TableClass
+{
+    public class TableStandard
+    {
+        public int Id { get; set; }
+
+        public string MessageOne { get; set; }
+
+        public string MessageTwo { get; set; }
+
+        public string MemoOne { get; set; }
+
+        public string MemoTwo { get; set; }
+
+        public DateTime? CreateDatetime { get; set; }
+
+        public DateTime? UpdatedDatetime { get; set; }
+    }
+}
---
+++ HannahDatabase/Security/RoleMemberships.sql
@@ -0,0 +1,18 @@
+﻿ALTER ROLE [db_owner] ADD MEMBER [cyrilyen];
+
+
+GO
+ALTER ROLE [db_owner] ADD MEMBER [kyle];
+
+
+GO
+ALTER ROLE [db_owner] ADD MEMBER [adam];
+
+
+GO
+ALTER ROLE [db_datareader] ADD MEMBER [hannah];
+
+
+GO
+ALTER ROLE [db_datawriter] ADD MEMBER [hannah];
+
